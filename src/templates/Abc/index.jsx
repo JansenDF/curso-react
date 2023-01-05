2f7c4ad4c0a6c49fd818b05b2ef356512ffa0dab
Jansen Lima <jansen.lima.df@gmail.com>
2023-01-05 17:42:13 -0300

    Adicionado useHistory e useLocation

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,10 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { useHistory, useLocation, useParams } from "react-router-dom"
 
 export const Abc = () => {
 
     const params = useParams()
+    const location = useLocation()
+    const history = useHistory()
     
     console.log(params)
+    console.log(location)
+    console.log(history)
+
+    useEffect(()=>{
+        setTimeout(function(){
+            history.push("/")
+        },[5000])
+    },[history])
     return(
         <h1>ABC</h1>
     )
